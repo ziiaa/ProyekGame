dcb38096c11585c46651eff7b00df22ea22bf0c1
ziiaa <azzizahnur01@srudents.amikom.ac.id>
2024-06-10 15:35:29 +0700

    Add Loading

---
+++ Assets/Loading Bar in Unity3D/scene_loader.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class scene_loader : MonoBehaviour
+{
+    public Image loadingfill;
+    public float minimumLoadingTime = 3f; // Minimum time the loading should take
+
+    void Start()
+    {
+        StartCoroutine(Loading());
+    }
+
+    IEnumerator Loading()
+    {
+        float elapsedTime = 0f;
+        AsyncOperation loading = SceneManager.LoadSceneAsync("Scene 2");
+
+        // Prevent the scene from activating until we allow it
+        loading.allowSceneActivation = false;
+
+        while (!loading.isDone)
+        {
+            // Calculate the progress based on elapsed time and actual loading progress
+            float progress = Mathf.Clamp01(elapsedTime / minimumLoadingTime);
+            loadingfill.fillAmount = Mathf.Lerp(loadingfill.fillAmount, progress, Time.deltaTime);
+
+            // Increment the elapsed time
+            elapsedTime += Time.deltaTime;
+
+            // If the operation is almost done and the minimum loading time has passed, allow the scene to activate
+            if (loading.progress >= 0.9f && elapsedTime >= minimumLoadingTime)
+            {
+                loading.allowSceneActivation = true;
+            }
+
+            yield return null;
+        }
+    }
+}
